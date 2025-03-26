29c191c79795daefd9569beb87826625fa0ed2cb
anthony peter <www.anthonygoldpeter@gmail.com>
2025-03-26 04:30:31 -0700

    renameing

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,10 @@
-export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://localhost:5000";
+let baseUrl;
+
+if (process.env.NODE_ENV === "development") {
+    baseUrl = "http://localhost:5000";
+} else {
+    baseUrl = "https://nodesockect.onrender.com/";
+}
+
+export default baseUrl;
